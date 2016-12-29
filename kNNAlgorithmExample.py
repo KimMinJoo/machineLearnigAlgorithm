54c328a415b148da6e9fceda53d78161b62aac93
kim-minjoo <kim.minjoo@nhn.com>
2016-12-29 15:55:07 +0900

    matplotlib을 이용한 시각적으로 matrix표현 및 matplotlib을 라이브러리목록에 추가. 주의 사항으로는 tcl이 virutalenv버전이 낮을경우 복사가 되지않는다. 이때 python폴더에서 virtualenv폴더로 복사해오면 해결된다.

--- kNNAlgorithmExample.py
+++ kNNAlgorithmExample.py
@@ -1,10 +1,14 @@
 from numpy import *
+import matplotlib
+import matplotlib.pyplot as plt
+import bookkNNAlgorithm
 
 def file2matrix(filename):
     fr = open(filename)
     numberOfLines = len(fr.readlines())
     returnMat = zeros((numberOfLines, 3))
     classLabelVector = []
+    fr = open(filename)
     index = 0
     for line in fr.readlines():
         line = line.strip()
@@ -14,3 +18,12 @@ def file2matrix(filename):
         index += 1
     return returnMat, classLabelVector
 
+datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+
+print(datingDataMat)
+print(datingLabels)
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2])
+plt.show()
